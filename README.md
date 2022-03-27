ad2eea4edf998be264567d39dc870e0bc6b18ba7
Swarnesh Ravi Jha <srjha@q3infotech.net>
2022-03-27 13:31:40 +0530

    git initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+**Chat Web Application**
+
+Requirements: Java 11, Maven, Websocket
+
+This spring boot websocket application allows users to chat with each other in real time.
---
+++ src/main/java/chatapplication/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package chatapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // SockJS is used to enable fallback options for browsers that don’t support websocket.
+        // STOMP stands for Simple Text Oriented Messaging Protocol.
+        // It is a messaging protocol that defines the format and rules for data exchange.
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+        // Enables a simple in-memory broker
+    }
+
+}
